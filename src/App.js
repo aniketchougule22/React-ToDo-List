59a08750349210f212010ae1cb4e470f04a25bfc
aniketchougule22 <chouguleaniket03@gmail.com>
2023-03-10 17:04:27 +0530

    UI designing and functionality part Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
+import List from './List';
 
-function App() {
+const App = () => {
+
+  const [inputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value)
+  }
+
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    })
+    setInputList("");
+  }
+
+  const deleteItems = (id) => {
+    // console.log('deleted');
+    setItems((oldItems) => {
+      return oldItems.filter((element, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='main_div'>
+        <div className='center-div'>
+          <br/>
+          <h1>ToDo List</h1>
+          <br/>
+          <input 
+            type='text' 
+            placeholder='Add a Items'
+            value={inputList}
+            onChange={itemEvent}
+          />
+          <button onClick={listOfItems}> + </button>
+
+          <ol>
+            {
+              items.map((itemValue, index) => {
+                return <List 
+                          key={index}
+                          id={index}
+                          text={itemValue}
+                          onSelect={deleteItems}
+                        />
+              })
+            }
+          </ol>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/List.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const List = (props) => {
+
+    return (
+        <>
+            <div className="todo-style">
+                <i 
+                    className="fa fa-times" 
+                    aria-hidden="true"
+                    onClick={() => {
+                        props.onSelect(props.id);
+                    }}
+                />
+                <li>{props.text}</li>
+            </div>
+        </>
+    )
+}
+
+export default List;
